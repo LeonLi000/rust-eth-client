86aa70f9d212883aeebc22bc0176b775e5ab769e
LeonLi000 <matrix.skygirl@gmail.com>
2020-09-16 15:39:00 +0800

    feat: finish chain reorg logic

--- contracts/eth-client/src/logic.rs
+++ contracts/eth-client/src/logic.rs
@@ -8,10 +8,11 @@ use ckb_std::{
 };
 use molecule::prelude::{Reader, Builder, Entity};
 use eth_spv_lib::eth_types::*;
-use crate::types::basic::{BytesVec, HeaderInfo, HeaderInfoReader};
+use crate::types::basic::{BytesVec, HeaderInfo, HeaderInfoReader, BytesVecReader};
 use rlp::Encodable;
 
-pub const HEADER_CACHE_LIMIT: usize = 500;
+pub const MAIN_HEADER_CACHE_LIMIT: usize = 500;
+pub const UNCLE_HEADER_CACHE_LIMIT: usize = 500;
 
 #[derive(Debug)]
 pub struct CellDataTuple(Option<CellDataView>, Option<CellDataView>);
@@ -76,6 +77,30 @@ fn to_u64(data: &Uint64) -> u64 {
     u64::from_be_bytes(res)
 }
 
+fn traverse_uncle_chain(uncle_input_reader:BytesVecReader, mut current_hash: H256, mut number:u64) -> Result<(), Error>{
+    let mut index = uncle_input_reader.len()-1;
+    loop {
+        if index == 0 {
+            return Err(Error::InvalidCellData);
+        }
+        let uncle_tail_input = uncle_input_reader.get_unchecked(index).raw_data();
+        if HeaderInfoReader::verify(&uncle_tail_input, false).is_err() {
+            return Err(Error::InvalidCellData);
+        }
+        let uncle_header_info_reader = HeaderInfoReader::new_unchecked(uncle_tail_input);
+        if uncle_header_info_reader.hash().raw_data() == current_hash.0.as_bytes() {
+            let uncle_header_raw = uncle_header_info_reader.header().raw_data();
+            let uncle_header: BlockHeader = rlp::decode(uncle_header_raw.to_vec().as_slice()).unwrap();
+            number -= 1;
+            current_hash = uncle_header.parent_hash;
+            break;
+        } else {
+            index -= 1;
+        }
+    }
+    Ok(())
+}
+
 fn verify_input_output_data(input: &CellDataView, output: &CellDataView, header_raw: &[u8]) -> Result<BlockHeader, Error> {
     let header: BlockHeader = rlp::decode(header_raw.to_vec().as_slice()).unwrap();
     debug!("header after decode is {:?}", header);
@@ -96,110 +121,124 @@ fn verify_input_output_data(input: &CellDataView, output: &CellDataView, header_
     debug!("main_output len: {:?}", main_output_reader.len());
     // let main_tail = main_output_reader.get_unchecked(main_output_reader.len()-1).raw_data();
     // header is on main chain.
-    let tail_info_input = main_input_reader.get_unchecked(main_input_reader.len() - 1).raw_data();
-    if HeaderInfoReader::verify(&tail_info_input, false).is_err() {
+    let main_tail_info_input = main_input_reader.get_unchecked(main_input_reader.len() - 1).raw_data();
+    if HeaderInfoReader::verify(&main_tail_info_input, false).is_err() {
         return Err(Error::InvalidCellData);
     }
-    let tail_info_input_reader = HeaderInfoReader::new_unchecked(tail_info_input);
-    let tail_header_input = tail_info_input_reader.header().raw_data();
+    let main_tail_info_input_reader = HeaderInfoReader::new_unchecked(main_tail_info_input);
+    let main_tail_header_input = main_tail_info_input_reader.header().raw_data();
 
-    let tail_info_output = main_output_reader.get_unchecked(main_output_reader.len() - 1).raw_data();
-    if HeaderInfoReader::verify(&tail_info_output, false).is_err() {
+    let main_tail_info_output = main_output_reader.get_unchecked(main_output_reader.len() - 1).raw_data();
+    if HeaderInfoReader::verify(&main_tail_info_output, false).is_err() {
         return Err(Error::InvalidCellData);
     }
-    debug!("tail_info_output: {:?}", tail_info_output);
-    let tail_info_output_reader = HeaderInfoReader::new_unchecked(tail_info_output);
-    let tail_header_output = tail_info_output_reader.header().raw_data();
+    debug!("main_tail_info_output: {:?}", main_tail_info_output);
+    let main_tail_info_output_reader = HeaderInfoReader::new_unchecked(main_tail_info_output);
+    let main_tail_header_output = main_tail_info_output_reader.header().raw_data();
+
     // header is on main chain.
-    if tail_header_output == header_raw {
+    if main_tail_header_output == header_raw {
         debug!("header is on main chain");
-        let tail_input: BlockHeader = rlp::decode(tail_header_input.to_vec().as_slice()).unwrap();
-        debug!("header parent hash: {:?} tail_input hash: {:?}", header.parent_hash.0, tail_input.hash.unwrap().0);
+        assert_eq!(main_tail_info_output_reader.hash().raw_data(), header.hash.unwrap().0.as_bytes());
+        let main_tail_input: BlockHeader = rlp::decode(main_tail_header_input.to_vec().as_slice()).unwrap();
+        debug!("header parent hash: {:?} tail_input hash: {:?}", header.parent_hash.0, main_tail_input.hash.unwrap().0);
         // if header.parent_hash == tail_input.hash => the chain is not reorg.
         // else do reorg.
-        if tail_input.hash.unwrap().0 == header.parent_hash.0 {
+        if main_tail_input.hash.unwrap() == header.parent_hash {
             debug!("the chain is not reorg.");
-            let prev_difficult: Uint64 = tail_info_input_reader.total_difficulty().to_entity();
-            let left: Uint64 = tail_info_output_reader.total_difficulty().to_entity();
+            let prev_difficult: Uint64 = main_tail_info_input_reader.total_difficulty().to_entity();
+            let left: Uint64 = main_tail_info_output_reader.total_difficulty().to_entity();
             let right: Uint64 = header.difficulty.0.as_u64().into();
             debug!("left u64: {} right u64 {}", to_u64(&left), to_u64(&right).checked_add(to_u64(&prev_difficult)).unwrap());
             assert_eq!(to_u64(&left), to_u64(&right).checked_add(to_u64(&prev_difficult)).unwrap());
 
-            if main_output_reader.len() > HEADER_CACHE_LIMIT {
+            if main_output_reader.len() > MAIN_HEADER_CACHE_LIMIT {
                 return Err(Error::InvalidCellData);
             }
-            if main_input_reader.len() == main_output_reader.len() && main_output_reader.len() == HEADER_CACHE_LIMIT {
-                let mut input_data = vec![];
-                for i in 1..main_input_reader.len() {
-                    input_data.push(main_input_reader.get_unchecked(i).raw_data())
+            verify_original_chain_data(main_input_reader, main_output_reader, MAIN_HEADER_CACHE_LIMIT)?;
+            // the uncle chain should be the same.
+            assert_eq!(uncle_input_reader.as_slice(),uncle_output_reader.as_slice());
+        } else {
+            debug!("the chain is reorg.");
+            let left: Uint64 = main_tail_info_input_reader.total_difficulty().to_entity();
+            let right: Uint64 = main_tail_info_output_reader.total_difficulty().to_entity();
+            if to_u64(&right) >= to_u64(&left) {// header.number < main_tail_input.number
+                // assert_eq!(main_tail_input.number - header.number > 0, true)
+                let mut number = header.number - 1;
+                let mut current_hash = header.parent_hash;
+                loop {
+                    if number == 0 {
+                        return Err(Error::InvalidCellData);
+                    }
+                    // find parent header.
+                    if main_tail_input.number <= number { // the parent header is on uncle chain.
+                        traverse_uncle_chain(uncle_input_reader, current_hash, number)?;
+                    } else {
+                        let offset = (main_tail_input.number - number-1) as usize;
+                        if offset > main_input_reader.len() {
+                            return Err(Error::InvalidCellData);
+                        }
+                        let header_info_temp = main_input_reader.get_unchecked(main_input_reader.len()-1-offset).raw_data();
+                        if HeaderInfoReader::verify(&header_info_temp, false).is_err() {
+                            return Err(Error::InvalidCellData);
+                        }
+                        let header_info_temp_reader = HeaderInfoReader::new_unchecked(header_info_temp);
+                        // let header_temp: BlockHeader = rlp::decode(header_temp.to_vec().as_slice()).unwrap();
+                        if header_info_temp_reader.hash().raw_data() == current_hash.0.as_bytes() {// the parent header is on main chain.
+                            let mut input_data = vec![];
+                            for i in 1..main_input_reader.len()-1-offset {
+                                input_data.push(main_input_reader.get_unchecked(i).raw_data())
+                            }
+                            let mut output_data = vec![];
+                            for i in 0..main_output_reader.len()-1 {
+                                output_data.push(main_output_reader.get_unchecked(i).raw_data())
+                            }
+                            assert_eq!(input_data, output_data);
+                            break;
+                        } else {// the parent header is on uncle chain.
+                            traverse_uncle_chain(uncle_input_reader, current_hash, number)?;
+                        }
+                    }
                 }
-                let mut output_data = vec![];
-                for i in 0..main_output_reader.len()-1 {
-                    output_data.push(main_output_reader.get_unchecked(i).raw_data())
-                }
-                assert_eq!(input_data, output_data);
-            } else if main_input_reader.len() < main_output_reader.len(){
-                debug!("the main input len < output len");
-                let mut input_data = vec![];
-                for i in 0..main_input_reader.len() {
-                    input_data.push(main_input_reader.get_unchecked(i).raw_data())
-                }
-                let mut output_data = vec![];
-                for i in 0..main_output_reader.len()-1 {
-                    output_data.push(main_output_reader.get_unchecked(i).raw_data())
-                }
-                assert_eq!(input_data, output_data);
             } else {
                 return Err(Error::InvalidCellData);
             }
-        } else {
-            debug!("the chain is reorg.");
-
         }
+    } else {
+        // the header is on uncle chain. just do append.
+        verify_original_chain_data(uncle_input_reader, uncle_output_reader, UNCLE_HEADER_CACHE_LIMIT)?;
+        // the main chain should be the same.
+        assert_eq!(main_output_reader.as_slice(),main_input_reader.as_slice());
     }
-
-
-    // header is on uncle chain will cause chain org.
-    // let uncle_tail = uncle_output_reader.get_unchecked(uncle_input_reader.len()-1).raw_data();
-    // if main_input_reader.len() {
-    //
-    // }
-
-    // assert_eq!(header_ , header_raw);
-
-    // debug!("difficulty: {:?}",header_info_reader.total_difficulty().raw_data().into());
-    // let mut data = [0u8; 8];
-    // data.copy_from_slice(Uint64::from_slice(&header_info_reader.total_difficulty().raw_data()).expect(""));
-
-
-    // if main_output_reader.len() > main_input_reader.len() {
-    //     assert_eq!(main_output_reader.get_unchecked(main_output_reader.len() - 1).raw_data(), header_raw);
-    //     let mut input_data = vec![];
-    //     for i in 0..main_input_reader.len() {
-    //         input_data.push(main_input_reader.get_unchecked(i).raw_data())
-    //     }
-    //     input_data.push(header_raw);
-    //     let mut output_data = vec![];
-    //     for i in 0..main_output_reader.len() {
-    //         output_data.push(main_output_reader.get_unchecked(i).raw_data())
-    //     }
-    //     assert_eq!(input_data, output_data);
-    // } else if uncle_output_reader.len() > uncle_input_reader.len() {
-    //     assert_eq!(uncle_output_reader.get_unchecked(uncle_output_reader.len() - 1).raw_data(), header_raw);
-    //     let mut input_data = vec![];
-    //     for i in 0..uncle_input_reader.len() {
-    //         input_data.push(uncle_input_reader.get_unchecked(i).raw_data())
-    //     }
-    //     input_data.push(header_raw);
-    //     let mut output_data = vec![];
-    //     for i in 0..uncle_output_reader.len() {
-    //         output_data.push(uncle_output_reader.get_unchecked(i).raw_data())
-    //     }
-    //     assert_eq!(input_data, output_data);
-    // } else {
-    //     return Err(Error::InvalidCellData);
-    // }
+    assert_eq!(main_output_reader.get_unchecked(main_output_reader.len() - 1).raw_data(), header_raw);
     Ok(header)
+}
+
+fn verify_original_chain_data(uncle_input_reader: BytesVecReader, uncle_output_reader: BytesVecReader, limit: usize) -> Result<(), Error> {
+    if uncle_input_reader.len() == uncle_output_reader.len() && uncle_output_reader.len() == limit {
+        let mut input_data = vec![];
+        for i in 1..uncle_input_reader.len() {
+            input_data.push(uncle_input_reader.get_unchecked(i).raw_data())
+        }
+        let mut output_data = vec![];
+        for i in 0..uncle_output_reader.len()-1 {
+            output_data.push(uncle_output_reader.get_unchecked(i).raw_data())
+        }
+        assert_eq!(input_data, output_data);
+    } else if uncle_input_reader.len() < uncle_output_reader.len(){
+        let mut input_data = vec![];
+        for i in 0..uncle_input_reader.len() {
+            input_data.push(uncle_input_reader.get_unchecked(i).raw_data())
+        }
+        let mut output_data = vec![];
+        for i in 0..uncle_output_reader.len()-1 {
+            output_data.push(uncle_output_reader.get_unchecked(i).raw_data())
+        }
+        assert_eq!(input_data, output_data);
+    } else {
+        return Err(Error::InvalidCellData);
+    }
+    Ok(())
 }
 
 fn parse_proof(proof_raw: &[u8]) -> Result<DoubleNodeWithMerkleProof, Error> {
